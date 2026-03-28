5ec88282b462fb10cebf43d81938b2ef333559c0
willex2104 <40055162+willex2104@users.noreply.github.com>
2026-03-28 17:29:47 +0000

    Delete FrodOS directory

--- FrodOS/shell.h
+++
@@ -1,8 +0,0 @@
-// shell.h
-#ifndef SHELL_H
-#define SHELL_H
-
-void launch_shell();  // The function that starts the OS interface
-void print_welcome(); // The function for your new V0.04 screen
-
-#endif
